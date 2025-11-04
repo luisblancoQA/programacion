698b56452fedf95a364b563d9ae1027d07d8b441
luisblancoQA <luisblanco.qa@gmail.com>
2025-11-04 11:28:49 -0300

    Agrego homework lección 3

---
+++ # Tipos de datos básicos.py
@@ -0,0 +1,10 @@
+# Conversiones de tipos
+edad = "46"
+edad_numero = int(edad)   #de texto a número
+
+altura = 1.75
+altura_texto = str(altura)    #de número a texto
+
+print(edad_numero + 4)     #ahora sí podemos sumar
+print("Tu altura es " + altura_texto)
+
