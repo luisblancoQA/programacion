7ab9b53993dd000c3f113bdc1a1b6fffc7460a0b
luisblancoQA <luisblanco.qa@gmail.com>
2025-11-07 10:46:55 -0300

    mensaje

---
+++ leccion_3/ejercicios.py
@@ -0,0 +1,28 @@
+# Ejemplo
+nombre = "Luis"   
+edad = 46
+es_estudiante = True
+
+# Mis primeras variables
+nombre = "Luis"             # str
+edad = 46                   # int
+altura = 1.75               # float
+es_estudiante = True        # bool
+
+print("Nombre:", nombre)
+print("Edad:", edad)
+print("Altura:", altura)
+print("¿Es estudiante?", es_estudiante)
+
+# Una variable puede cambiar su valor:
+puntaje = 0
+print("Puntaje inicial:", puntaje)
+
+puntaje = 10
+print("Nuevo puntaje:", puntaje)
+
+# Se le puede preguntar a Python de qué tipo es una variable
+x = 42
+print(type(x))
+
+
---
+++ leccion_3/homework3.py
@@ -0,0 +1,23 @@
+# Parte 1 _ Práctica básica
+nombre = "Luis"
+edad = 46
+ciudad = "Paysandú"
+aprendiendo = True
+
+print(f"Nombre: {nombre}, Edad: {edad}, Ciudad: {ciudad}, ¿Está estudiando Programación? {aprendiendo}")
+
+# Parte 2 _ Desafío
+saldo = 1500
+gasto_comida = 200
+gasto_estudio = 350
+
+saldo -= gasto_comida + gasto_estudio
+print(f"Después de los gastos, mi saldo es de {saldo} pesos")
+
+# Parte 3 _ Curioso
+valor = "25"
+print(type(valor))
+
+valor = 25
+print(type(valor))
+
