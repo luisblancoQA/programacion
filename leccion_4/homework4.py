38d155c6a3291dd5f68453a041b4013e747b34af
luisblancoQA <luisblanco.qa@gmail.com>
2025-11-12 08:59:27 -0300

    Agregué Homework4

---
+++ leccion_4/homework4.py
@@ -0,0 +1,24 @@
+# Ejercicio 1 - Clasificación
+year = 2025
+precio = 49.90
+producto = "helado"
+disponible = True
+
+print("Año:", year, type(year))
+print("Precio:", precio, type(precio))
+print("Producto", producto, type(producto))
+print("¿Está disponible?", disponible, type(disponible))
+
+# Ejercicio 2 - Conversión
+precio_texto = "49.99"
+precio_decimal = float(precio_texto)
+print(f"El precio es {precio_decimal + 10} pesos")
+
+# Ejericio 3 Mini biografía
+nombre = "Luis"
+edad = 46
+altura = 1.75
+es_estudiante = True
+
+print(f"Hola, me llamo {nombre}. Tengo {edad} años, mido {altura} metros y soy estudiante: {es_estudiante}")
+
